1f66ff75c346f64a541e0203411beb7657d405a9
Sasha Camargo <69942870+sashacamar@users.noreply.github.com>
2021-10-21 22:07:43 -0300

    Desafio 1

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//let nombre = prompt("PRIEDRA / PAPEL / TIJERA" + "\n \nIngresa tu nombre.");
+
+
+//document.write("<h1>" + "Hola " + nombre + "</h1>" + "<h1>" + "Espero estes de animo para jugar" + "</h1>" + "<h1>" + ":D" + "</h1>");
+
+//document.write("Hola " + nombre + "Espero estes de animo para jugar" + ":D" );
