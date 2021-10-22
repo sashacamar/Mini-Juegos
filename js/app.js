b0b1c8029b1ebcb57a4fca7298cadf2b056fa5f7
Sasha Camargo <69942870+sashacamar@users.noreply.github.com>
2021-10-21 23:47:08 -0300

    Desafio 2 - modificacion del js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,35 @@
 let jugador1 = prompt("PRIEDRA / PAPEL / TIJERA" + "\n \nIngresa tu nombre.");
-let opcion_jugador1 = prompt("Escoge!" + "\n \n- Piedra \n- Papel \n- Tijera");
+let opcion1 = prompt("Escoge!" + "\n \n1. Piedra \n2. Papel \n3. Tijera");
+let opcion_jugador1;
 
-document.write(`<div class='cuadro'> <h1> Jugador: <strong>${jugador1}</strong> </h1> <h1> Ha escogido <strong>${opcion_jugador1}</strong> </div>`);
+
+if (opcion1 != 'ESC') {
+    let mayus = opcion1.toUpperCase();
+
+    switch (mayus) {
+        case '1':
+        case 'PIEDRA':
+            opcion_jugador1 = 'Piedra';
+            document.write(`<div class='cuadro'> <h1> Jugador: <strong>${jugador1}</strong> </h1> <h1> Ha escogido <strong>${opcion_jugador1}</strong> </div>`);
+            break;
+
+        case '2':
+        case 'PAPEL':
+            opcion_jugador1 = 'Papel';
+            document.write(`<div class='cuadro'> <h1> Jugador: <strong>${jugador1}</strong> </h1> <h1> Ha escogido <strong>${opcion_jugador1}</strong> </div>`);
+            break;
+
+        case '3':
+        case 'TIJERA':
+            opcion_jugador1 = 'Tijera';
+            document.write(`<div class='cuadro'> <h1> Jugador: <strong>${jugador1}</strong> </h1> <h1> Ha escogido <strong>${opcion_jugador1}</strong> </div>`);
+            break;
+
+        default:
+            document.write(`<div class='cuadro'> <h1> Jugador: <strong>${jugador1}</strong> </h1> <h1> No ha elegido ninguna opcion</strong> </div>`);
+
+    }
+}
+
 
 
