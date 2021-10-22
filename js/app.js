4a4646a63f83e150fb95dbc71a68d9e610c3848e
Sasha Camargo <69942870+sashacamar@users.noreply.github.com>
2021-10-21 22:54:57 -0300

    desafio 2

--- js/app.js
+++ js/app.js
@@ -3,3 +3,4 @@ let opcion_jugador1 = prompt("Escoge!" + "\n \n- Piedra \n- Papel \n- Tijera");
 
 document.write(`<div class='cuadro'> <h1> Jugador: <strong>${jugador1}</strong> </h1> <h1> Ha escogido <strong>${opcion_jugador1}</strong> </div>`);
 
+
