8b97f13d5f88ce9dbcc6e124545530ef252f5462
Sasha Camargo <69942870+sashacamar@users.noreply.github.com>
2021-10-29 15:07:04 -0300

    Desafio 4 y 5 - se agrego funciones y objetos

--- js/app.js
+++ js/app.js
@@ -1,15 +1,76 @@
 let jugador1 = prompt("PRIEDRA / PAPEL / TIJERA" + "\n \nIngresa tu nombre.");
 let opcion_jugador1;
 
-let max_jugadas = prompt("Cuentas partidas deseas jugar?");
+let max_partidas = prompt("Cuentas partidas deseas jugar?");
+let numPartida;
 
 let opcion_pc;
+
+
 
 
 
 document.write(`<div class='cuadro_nombre'> 
 <h1> Jugador: <strong>${jugador1}</strong> </h1> 
 </div>`);
+
+class Partidas {
+    constructor (usuario, numPartida, eligio, pcAtaca) {
+        this.usuario = usuario;
+        this.numPartida = numPartida;
+        this.eligio = eligio;
+        this.pcAtaca = pcAtaca;
+        //this.resultado = resultado;
+    }
+    mostrarResultado  = function() {
+        if (this.eligio == "Piedra") {
+            if(this.pcAtaca == "Papel"){
+                this.resultado = "perdiste";
+            }else if (this.pcAtaca == "Tijera"){
+                this.resultado = "has_ganado";
+            } else {
+                this.resultado = "empate";
+            }
+        }
+    
+        if (this.eligio == "Papel") {
+            if(this.pcAtaca == "Tijera"){
+                this.resultado = "perdiste";
+            }else if (this.pcAtaca == "Piedra"){
+                this.resultado = "has_ganado";
+            } else {
+                this.resultado = "empate";
+            }
+        }
+    
+        if (this.eligio == "Tijera") {
+            if(this.pcAtaca == "Piedra"){
+                this.resultado = "perdiste";
+            }else if (this.pcAtaca == "Papel"){
+                this.resultado = "has_ganado";
+            } else {
+                this.resultado = "empate";
+            }
+        }
+    
+        if (this.eligio == "No has elegido ninguna opcion") {
+    
+                this.resultado = "perdiste";
+    
+        }
+    }
+    imprimirPartida = function() {
+        this.mostrarResultado();
+    
+        document.write(`<div class='cuadro'> 
+                <h1> <strong> Partida ${this.numPartida} </strong> </h1>
+                <h1> Has escogido <strong>${this.eligio}</strong> </h1> 
+                <h1> PC: Te ataca con <strong>${this.pcAtaca}</strong> </h1> 
+                <img class="imgResultado" src=assets/img/${this.resultado}.png>
+                </div>`);
+    }
+    
+}
 
 
 function pcAleatorio(max) {
@@ -30,55 +91,54 @@ function pcAleatorio(max) {
     }
   }
 
-for (let i = 1; i <= max_jugadas; i+=1) {
-    let opcion1 = prompt("Escoge!" + "\n \n1. Piedra \n2. Papel \n3. Tijera");
+
+
+
+
+
+
+for (let i = 0; i < max_partidas; i+=1) {
+    let opcion1 = prompt(`Partida ${i} \nEscoge! \n \n1. Piedra \n2. Papel \n3. Tijera`);
     
     let mayus = opcion1.toUpperCase();
+
+    numPartida = i + 1;
+
+    let partida = {};
 
     switch (mayus) {
         case '1':
         case 'PIEDRA':
             opcion_jugador1 = "Piedra";
-            pcAleatorio(3);
-            document.write(`<div class='cuadro'> 
-            <h1> <strong> Jugada ${i} </strong> </h1>
-            <h1> Has escogido <strong>${opcion_jugador1}</strong> </h1> 
-            <h1> PC: Te ataca con <strong>${opcion_pc}</strong> </h1> 
-            </div>`);
             break;
 
         case '2':
         case 'PAPEL':
             opcion_jugador1 = "Papel";
-            pcAleatorio(3);
-            document.write(`<div class='cuadro'>
-            <h1> <strong> Jugada ${i} </strong> </h1>
-            <h1> Has escogido <strong>${opcion_jugador1}</strong> </h1> 
-            <h1> PC: Te ataca con <strong>${opcion_pc}</strong> </h1> 
-            </div>`);
             break;
 
         case '3':
         case 'TIJERA':
             opcion_jugador1 = "Tijera";
-            pcAleatorio(3);
-            document.write(`<div class='cuadro'> 
-            <h1> <strong> Jugada ${i} </strong> </h1>
-            <h1> Has escogido <strong>${opcion_jugador1}</strong> </h1> 
-            <h1> PC: Te ataca con <strong>${opcion_pc}</strong> </h1> 
-            </div>`);
             break;
 
         default:
-            pcAleatorio(3);
-            document.write(`<div class='cuadro'> 
-            <h1> <strong> Jugada ${i} </strong> </h1>
-            <h1> No has elegido ninguna opcion</strong> </h1> 
-            <h1> PC: Te ataca con <strong>${opcion_pc}</strong> </h1> 
-            </div>`);
-
+            opcion_jugador1 = "No has elegido ninguna opcion";
     }
 
+    pcAleatorio(3);
+    partida[i] = new Partidas (jugador1, numPartida, opcion_jugador1, opcion_pc);
+    partida[i].imprimirPartida();
+
+    console.table(partida[i]);
+
 }
+
+
+
+
+
+
+
 
 
