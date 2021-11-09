decd4fccec2a426f3df8ff68dee7a73decf0f1c0
Sasha Camargo <69942870+sashacamar@users.noreply.github.com>
2021-11-09 14:20:48 -0300

    Desafio 8 - DOM

--- js/app.js
+++ js/app.js
@@ -1,38 +1,38 @@
-//Variables ---------------------------------------------------------------------------------
-
-let jugador1 = prompt("PRIEDRA / PAPEL / TIJERA" + "\n \nIngresa tu nombre.");
-let opcion_jugador1;
-
-let max_partidas = prompt("Cuentas partidas deseas jugar?");
-let numPartida;
-
-let opcion_pc;
-
 //Array ---------------------------------------------------------------------------------
 
 const partidas = [];
 
-
-//Document.write del nombre del jugador ------------------------------------------------------
-
-document.write(`<div class='cuadro_nombre'> 
-<h1> Jugador: <strong>${jugador1}</strong> </h1> 
-</div>`);
-
-
 //Constructor de objetos -------------------------------------------------------------------
 
 class Partida {
-    constructor (usuario, numPartida, eligio, pcAtaca) {
+    constructor (usuario, eligio) {
         this.usuario = usuario;
-        this.numPartida = numPartida;
         this.eligio = eligio;
-        this.pcAtaca = pcAtaca;
-        //this.resultado = resultado;
+        this.pcAtaca;
+        this.resultado;
     }
 
     //Metodos del objeto ---------------------------------------------------------------------
-    mostrarResultado() {
+    pcAleatorio(max) {
+        let num_aleatorio = Math.floor(Math.random() * max);
+    
+        switch (num_aleatorio) {
+            case 0:
+                this.pcAtaca = "Piedra";
+                break;
+            case 1:
+                this.pcAtaca = "Papel";
+                break;
+            case 2:
+                this.pcAtaca = "Tijera";
+                break;
+            default:
+                this.pcAtaca = "Hubo un error";
+        }
+      }
+    
+
+    calcularResultado() {
         if (this.eligio == "Piedra") {
             if(this.pcAtaca == "Papel"){
                 this.resultado = "perdiste";
@@ -69,80 +69,73 @@ class Partida {
     
         }
     }
+
+
     imprimirPartida() {
-        this.mostrarResultado();
+        this.calcularResultado();
     
-        document.write(`<div class='cuadro'> 
-                <h1> <strong> Partida ${this.numPartida} </strong> </h1>
-                <h1> Has escogido <strong>${this.eligio}</strong> </h1> 
-                <h1> PC: Te ataca con <strong>${this.pcAtaca}</strong> </h1> 
-                <img class="imgResultado" src=assets/img/${this.resultado}.png>
-                </div>`);
+        let vos = document.getElementById("vos");
+        let pc = document.getElementById("pc");
+        let resultadoPartida = document.getElementById("resultadoPartida");
+
+        vos.src = `assets/img/${this.eligio}_vos.png`;
+        pc.src = `assets/img/${this.pcAtaca}_pc.png`;
+        resultadoPartida.src = `assets/img/${this.resultado}.png`;
+
     }
     
 }
 
 
-//funciones ---------------------------------------------------------------------------------
-
-//Opcion de la pc de forma aleatoria
-function pcAleatorio(max) {
-    let num_aleatorio = Math.floor(Math.random() * max);
-
-    switch (num_aleatorio) {
-        case 0:
-            opcion_pc = "Piedra";
-            break;
-        case 1:
-            opcion_pc = "Papel";
-            break;
-        case 2:
-            opcion_pc = "Tijera";
-            break;
-        default:
-            opcion_pc = "Hubo un error";
-    }
-  }
-
-
-
-//Bucle ---------------------------------------------------------------------------------
-
-for (let i = 0; i < max_partidas; i+=1) {
-    let opcion1 = prompt(`Partida ${i+1} \nEscoge! \n \n1. Piedra \n2. Papel \n3. Tijera`);
+//Juego ---------------------------------------------------------------------------------
     
-    let mayus = opcion1.toUpperCase();
+    //Variables ---------------------------------------------------------------------------------
 
-    numPartida = i + 1;
+let jugador1 = prompt("PRIEDRA / PAPEL / TIJERA" + "\n \nIngresa tu nombre.");
+let opcion_jugador;
 
+let opcion = prompt(`${jugador1} Escoge! \n \n1. Piedra \n2. Papel \n3. Tijera`);
 
-    switch (mayus) {
-        case '1':
-        case 'PIEDRA':
-            opcion_jugador1 = "Piedra";
-            break;
+let mayus = opcion.toUpperCase();
+    
+    //Condicional ---------------------------------------------------------------------------------
 
-        case '2':
-        case 'PAPEL':
-            opcion_jugador1 = "Papel";
-            break;
+switch (mayus) {
+    case '1':
+    case 'PIEDRA':
+        opcion_jugador = "Piedra";
+        break;
 
-        case '3':
-        case 'TIJERA':
-            opcion_jugador1 = "Tijera";
-            break;
+    case '2':
+    case 'PAPEL':
+        opcion_jugador = "Papel";
+        break;
 
-        default:
-            opcion_jugador1 = "No has elegido ninguna opcion";
-    }
+    case '3':
+    case 'TIJERA':
+        opcion_jugador = "Tijera";
+        break;
 
-    pcAleatorio(3);
-    partidas.push(new Partida (jugador1, numPartida, opcion_jugador1, opcion_pc));
-    partidas[i].imprimirPartida();
-
-    console.table(partidas[i]);
-
+    default:
+        opcion_jugador = "No has elegido ninguna opcion";
 }
+
+
+    //Crear Objeto ---------------------------------------------------------------------------------
+
+partidas.push(new Partida (jugador1, opcion_jugador));
+
+    //llamando Metodos del objeto ------------------------------------------------------------------
+
+partidas[0].pcAleatorio(3);
+partidas[0].imprimirPartida();
+
+
+    //Consola -------------------------------------------------------------------------------------
+
+console.table(partidas[0]);
+
+
 
 
 
